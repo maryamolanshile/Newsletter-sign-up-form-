48bd8dbe7bb962bbb862f1eec49a3dccf223b9e5
Maryam Ismaeel <ismaeelmaryam11@gmail.com>
2026-01-06 23:11:03 +0100

    Started JavaScript for day 6

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-   
+ const form = document.getElementById("newsletter-signup");
+ const emailInput = document.getElementById("email")
+ const sucessMessage = document.getElementById("sucess-message")
