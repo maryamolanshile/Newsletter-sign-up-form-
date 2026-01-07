28ea7a9036e2c8f2d25dc45cde5fb0e3b50b4c70
Maryam Ismaeel <ismaeelmaryam11@gmail.com>
2026-01-07 23:04:27 +0100

    Started sucess message for day 7

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
  const form = document.getElementById("newsletter-signup");
- const emailInput = document.getElementById("email")
+ const emailInput = document.getElementById("email-input")
+ const submitBtn = document.getElementById("submit-button")
+ const userEmail = document.getElementById("user-email")
+ userEmail.textContent = emailInput.ariaValueMax
  const sucessMessage = document.getElementById("sucess-message")
