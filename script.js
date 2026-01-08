58124f713b9cd72dfa6a87bbc22675137f3fc11c
Maryam Ismaeel <ismaeelmaryam11@gmail.com>
2026-01-08 23:14:50 +0100

    created and styled the page for the sucess message after subscription

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
- const form = document.getElementById("newsletter-signup");
- const emailInput = document.getElementById("email-input")
- const submitBtn = document.getElementById("submit-button")
- const userEmail = document.getElementById("user-email")
- userEmail.textContent = emailInput.ariaValueMax
- const sucessMessage = document.getElementById("sucess-message")
+ const form = document.getElementById("newsletter-signup-form");
+ const emailInput = document.getElementById("email-input");
+ const submitBtn = document.getElementById("submit-button");
+ const userEmail = document.getElementById("user-email");
+ userEmail.textContent = emailInput.value;
+ const sucessMessage = document.getElementById("success-message");
+ const dismissBtn = document.getElementById("dismiss-button")
+
+ submitBtn.addEventListener("click", 
+    function (event) {
+        event.preventDefault()
+form.style.display = "none"
+sucessMessage.style.display = "block"
+    })
